1a1f5ea5064d8011b054ac4345ccbb53a5438e88
Fuat Can ERYİĞİT <fceryigit03@gmail.com>
2022-07-16 22:54:33 +0300

    he

--- Assets/Scripts/Objects/PooledObjectMovement.cs
+++ Assets/Scripts/Objects/PooledObjectMovement.cs
@@ -1,13 +1,34 @@
 using RedPanda.ObjectPooling;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class PooledObjectMovement : MonoBehaviour
 {
     [SerializeField] private SO_PooledObject _pooledObject;
+    [SerializeField] private SO_SpriteHolder _spriteHolder;
     [SerializeField] private float _movementSpeed = 5f;
 
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void Update()
     {
         transform.Translate(Vector3.back * Time.deltaTime * _movementSpeed);
+    }
+
+    private void OnEnable()
+    {
+        if (_pooledObject.PooledObjectTag == "Dice")
+        {
+            //deðiþtir burayý
+            _spriteRenderer.sprite = _spriteHolder.Sprites[0];
+        }
+        else if (_pooledObject.PooledObjectTag == "Rock")
+        {
+            _spriteRenderer.sprite = _spriteHolder.Sprites[0];
+        }
     }
 }
---
+++ Assets/Scripts/Objects/SO_SpriteHolder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SpriteHolder", menuName = "SpriteHolder")]
+public class SO_SpriteHolder : ScriptableObject
+{
+    [SerializeField] private Sprite[] _sprites;
+    public Sprite[] Sprites { get => _sprites; }
+}
